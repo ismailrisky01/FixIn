13256baac17be2dadd5cc2839d5ada72c78c6a11
ismailrisky <ismailrisky01@gmail.com>
2021-04-08 19:27:00 +0700

    8 April

--- app/src/main/java/com/example/fixin/ui/riwayat/RiwayatAdaper.kt
+++ app/src/main/java/com/example/fixin/ui/riwayat/RiwayatAdaper.kt
@@ -1,13 +1,13 @@
 package com.example.fixin.ui.riwayat
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fixin.databinding.ItemRiwayatBinding
-import java.util.*
 import kotlin.collections.ArrayList
 
-class RiwayatAdaper(val data: ArrayList<ModelRiwayat>) :
+class RiwayatAdaper(val context: Context, val data: ArrayList<ModelRiwayat>) :
     RecyclerView.Adapter<RiwayatAdaper.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemRiwayatBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -15,11 +15,14 @@ class RiwayatAdaper(val data: ArrayList<ModelRiwayat>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val data = data[position]
+        holder.binding.IDItemRiwayatNama.text = data.nama
+        holder.binding.IDItemRiwayatRating.rating = data.rating
+        holder.binding.IDItemRiwayatJam.text = data.time
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return data.size
     }
 
     class ViewHolder(val binding: ItemRiwayatBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -27,4 +30,4 @@ class RiwayatAdaper(val data: ArrayList<ModelRiwayat>) :
     }
 }
 
-class ModelRiwayat(val nama: String, val rating: String, val time: Date)
+class ModelRiwayat(val nama: String, val rating: Float, val time: String)
